be8f2aaee1c7d38bd207784d4cf524db118a5acc
deionCube <filedecoder@gmail.com>
2022-08-22 09:46:03 +0800

    Update Base64Ext.java

--- app/src/main/java/com/github/catvod/utils/Base64Ext.java
+++ app/src/main/java/com/github/catvod/utils/Base64Ext.java
@@ -1,7 +1,7 @@
 package com.github.catvod.utils;
 
 
- *
+/**
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
